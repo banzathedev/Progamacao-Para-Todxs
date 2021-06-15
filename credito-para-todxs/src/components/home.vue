be9575252a7a2c5d1eccaf710cc498f766e4bbcf
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-06-15 09:30:24 -0300

    commit

---
+++ credito-para-todxs/src/components/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1> home ta up </h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ credito-para-todxs/src/components/template/header.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-toolbar dense >
+        <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link Two</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
+
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
